533509709b5667aeb9236a57f65b2862f4fd296c
edsay12 <edvandearaujo2@hotmail.com>
2022-06-07 22:40:31 -0300

    pas001

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
+import imageOff from './assets/icons/eco-light-off.png'
+import imageOn from './assets/icons/eco-.png'
+import Torch from 'react-native-torch'
+import RNshake from 'react-native-shake'
 
 export default function App() {
+  const [toggle,setToggle] = useState(false)
+
+  function handletoggle(){
+    
+    toggle ? setToggle(false): setToggle(true)
+
+  }
+  useEffect(()=>{
+    Torch.switchState(true)
+  },[toggle])
+  useEffect(()=>{
+    const subscription = RNshake.addListener(()=>{
+      handletoggle()
+    });
+    return ()=> subscription.remove()
+  })
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={toggle ? styles.container : styles.containerLight}>
+      <Pressable onPress={handletoggle}>
+        <Image  style={styles.lightOn} source={toggle ? imageOff : imageOn} />
+
+      </Pressable>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +38,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  containerLight: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  lightOn: {
+    resizeMode: 'contain',
+    width: 150,
+    height: 150,
+  }
 });
